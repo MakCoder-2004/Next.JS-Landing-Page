a2808e5edb8c6bda02b6a96bf6bcb67d3e9ef13a
Makrious-Ayman <maaakrious2004@gmail.com>
2025-04-28 23:30:45 +0300

    Call To Action Section is finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Introduction from "@/sections/Introduction";
 import Features from "@/sections/Features";
 import Integrations from "@/sections/Integrations";
 import Faqs from "@/sections/Faqs";
+import CallToAction from "@/sections/CallToAction";
 
 
 export default function Home() {
@@ -17,6 +18,7 @@ export default function Home() {
       <Features/>
       <Integrations/>
       <Faqs/>
+      <CallToAction/>
     </div>
   );
 }
