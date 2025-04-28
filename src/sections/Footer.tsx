9c6c8559ecdc1f307b6f71b8c21cf6eea12ed02e
Makrious-Ayman <maaakrious2004@gmail.com>
2025-04-28 23:42:23 +0300

    Footer section is finished

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -4,6 +4,24 @@ const footerLinks = [
     { href: "#", label: "Terms & Conditions" },
 ];
 
+import LogoImage from "@/assets/images/logo.svg";
+import Image from "next/image";
+
 export default function Footer() {
-    return <div>Footer</div>;
+    return (
+        <section className="pt-24 pb-6">
+            <div className="p-12 flex flex-col md:flex-row md:justify-between items-center gap-6">
+                <div>
+                    <Image src={LogoImage} alt="Layers Logo" />
+                </div>
+                <div className="">
+                    {footerLinks.map(link => (
+                        <a key={link.label} href={link.href} className="text-neutral-500 hover:text-neutral-300 transition-colors duration-200 p-4">
+                            {link.label}
+                        </a>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
